1fcaa801f67b919460776a5c3473f9d46d14c8db
Bhuvaneshwar G <blackbhuvaneshwar@gmail.com>
2025-12-15 20:45:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post("/start-container", (req, res) => {
       docker rm -f vnc_ubuntu 2>/dev/null || true &&
       docker run -d -p 6081:6081 \
         --name vnc_ubuntu \
-        -v /home/bhuvanesh/Documents/docker/vnc_ubuntu/persistence_storage/ubuntu:/root/Desktop/student \
+        -v /home/ubuntu/ubuntu:/root/Desktop/student \
         vnc_ubuntu
     `;
     url = "/vnc/ubuntu/vnc.html";
@@ -30,7 +30,7 @@ app.post("/start-container", (req, res) => {
       docker rm -f vnc_kali 2>/dev/null || true &&
       docker run -d -p 6080:6080 \
         --name vnc_kali \
-        -v /home/bhuvanesh/Documents/docker/vnc_ubuntu/persistence_storage/kali:/root/Desktop/student \
+        -v /home/ubuntu/kali:/root/Desktop/student \
         vnc_kali
     `;
     url = "/vnc/kali/vnc.html";
